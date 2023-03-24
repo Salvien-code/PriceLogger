45e48ce57e2b6aff0d93c5c858584b3d5059ccaf
Simon Samuel <salviensky@gmail.com>
2023-03-24 02:55:39 +0100

    feat: implement counter contract and writing test

--- near/src/lib.rs
+++ near/src/lib.rs
@@ -1,18 +1,60 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::near_bindgen;
+use near_sdk::{env, near_bindgen, AccountId};
 
 #[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize, Default)]
+#[derive(BorshDeserialize, BorshSerialize)]
 pub struct Counter {
+    owner_id: AccountId,
     value: u64,
 }
 
 #[near_bindgen]
 impl Counter {
-    pub fn increment(&mut self) {
-        self.value += 1;
+    /**
+     * Initializes the contract with the given `owner_id`.
+     */
+    #[init]
+    pub fn new() -> Self {
+        Self {
+            owner_id: env::predecessor_account_id(),
+            value: 0,
+        }
     }
 
+    /**
+     * Only the owner of the contract can call this method.
+     */
+    pub fn reset(&mut self) {
+        assert!(
+            env::predecessor_account_id() == self.owner_id,
+            "Only owner can reset the counter."
+        );
+        self.value = 0;
+    }
+
+    /**
+     * Increment the counter by 1 and reset to 0 if it reaches 10.
+     */
+    pub fn increment(&mut self) {
+        match self.value {
+            0..=9 => self.value += 1,
+            _ => self.value = 0,
+        }
+    }
+
+    /**
+     * Decrement the counter by 1 and reset to 10 if it reaches 0.
+     */
+    pub fn decrement(&mut self) {
+        match self.value {
+            1..=10 => self.value -= 1,
+            _ => self.value = 10,
+        }
+    }
+
+    /**
+     * Get the current value of the counter.
+     */
     pub fn get_count(&self) -> u64 {
         self.value
     }
@@ -20,21 +62,27 @@ impl Counter {
 
 #[cfg(all(test, not(target_arch = "wasm32")))]
 mod tests {
-    use super::*;
-    use near_sdk::test_utils::VMContextBuilder;
-    use near_sdk::{testing_env, VMContext};
+    #![allow(unused_imports)]
 
-    fn get_context(is_view: bool) -> VMContext {
-        VMContextBuilder::new()
-            .signer_account_id("bob_near".parse().unwrap())
-            .is_view(is_view)
-            .build()
+    use near_sdk::test_utils::{accounts, VMContextBuilder};
+    use near_sdk::{testing_env, AccountId};
+
+    use super::*;
+
+    fn get_context(precedessor_account_id: AccountId, is_view: bool) -> VMContextBuilder {
+        let mut builder = VMContextBuilder::new();
+        builder
+            .signer_account_id(precedessor_account_id.clone())
+            .is_view(is_view);
+
+        builder
     }
 
     #[test]
-    fn my_test() {
-        let context = get_context(false);
-        testing_env!(context);
-        // ... Write test here
+    fn test_stores_owner() {
+        let context = get_context(accounts(1), false);
+        testing_env!(context.build());
+        let contract = Counter::new();
+        assert_eq!(contract.owner_id, accounts(1));
     }
 }
